88d8233001556569bdd30e767b81c3173eb94f28
Sana Tariq <sanatariq001122@gmail.com>
2025-01-08 23:30:16 +0500

    forgot password page completed

--- cypress/e2e/forgotPasswordpage.cy.js
+++ cypress/e2e/forgotPasswordpage.cy.js
@@ -1,6 +1,6 @@
 /// <reference types='cypress'/>
 
-describe('', () => {
+describe('Test cases for forgot password page', () => {
     it('Forgot Password Page', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.url().should('include', '/web/index.php/auth/login')    //to confirm the login path after base url
@@ -12,7 +12,11 @@ describe('', () => {
         cy.url().should('include', '/web/index.php/auth/requestPasswordResetCode') //to confirm new 'forgot password page'
         cy.get('h6').should('have.text', 'Reset Password')
         cy.get('input[name="username"]').type('Admin')
-        
+        cy.get('button[type="button"]')
+        .invoke("removeAttr", "target")
+        .click()
+        cy.url().should('include', '/web/index.php/auth/login')    //to confirm the page is navigated back to login path after base url
+
     });
 });
 
